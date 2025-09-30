55e4dca3088f7743bfb7afc9c26cf4bbb6a5bc4a
TomerP750 <tomer.jcb65@gmail.com>
2025-09-30 15:49:19 +0300

    imporved form

--- backend/src/main/java/com/grouply/backend/project_post/dto/CreateProjectPostDTO.java
+++ backend/src/main/java/com/grouply/backend/project_post/dto/CreateProjectPostDTO.java
@@ -1,8 +1,11 @@
 package com.grouply.backend.project_post.dto;
 
 import com.grouply.backend.project.Dtos.ProjectDTO;
+import com.grouply.backend.project_member.ProjectPosition;
+import com.grouply.backend.project_post_position.dto.ProjectPostPositionDTO;
 import lombok.Data;
 
+import java.util.List;
 
 
 @Data
@@ -10,6 +13,7 @@ public class CreateProjectPostDTO {
 
     private String title;
     private String description;
+    private List<ProjectPosition> positions;
     private Long projectId;
 
 }
--- frontend/src/dtos/models_dtos/request_dto/CreateProjectPostDTO.ts
+++ frontend/src/dtos/models_dtos/request_dto/CreateProjectPostDTO.ts
@@ -1,14 +1,17 @@
+import type { ProjectPosition } from "../../enums/ProjectPosition"
 
 
 export class CreateProjectPostDTO {
 
     title: string
     description: string
+    positions: ProjectPosition[]
     projectId: number
 
-    constructor(title: string, description: string, projectId: number) {
+    constructor(title: string, description: string,positions: ProjectPosition[] ,projectId: number) {
         this.title = title
         this.description = description
+        this.positions = positions
         this.projectId = projectId
     }
  
