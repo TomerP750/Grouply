77d590a7dac1f0f54e47bdcc617eb7f31cd920be
TomerP750 <tomer.jcb65@gmail.com>
2025-10-01 22:22:54 +0300

    improved dashboard navlinks

--- frontend/src/components/layout/Routing.tsx
+++ frontend/src/components/layout/Routing.tsx
@@ -9,6 +9,8 @@ import { ProfilePage } from "../pages/profile/profile_page";
 import { useUserSelector } from "../../redux/hooks";
 import { PostPage } from "../pages/projects-area/project_info_page/post_page";
 import { Dashboard } from "../pages/dashboard/dashboard";
+import { Overview } from "../pages/dashboard/overview";
+import { ProjectsDashboard } from "../pages/dashboard/projects";
 
 
 export function Routing() {
@@ -18,20 +20,23 @@ export function Routing() {
     return (
         <div>
             <Routes>
-                <Route path="/" element={user ? <SearchProjectsPage/> : <Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/signup" element={<SignUpWizard/>}/>
+                <Route path="/" element={user ? <SearchProjectsPage /> : <Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUpWizard />} />
 
-                <Route path="/profile/:id" element={<ProfilePage/>}/>
-                <Route path="/dashboard/:id" element={<Dashboard/>}/>
-                
-                <Route path="/post/:id" element={<PostPage/>}/>
-                <Route path="/search-projects" element={<SearchProjectsPage/>}/>
+                <Route path="/profile/:id" element={<ProfilePage />} />
+                <Route path="/dashboard/:id" element={<Dashboard />}>
+                    <Route index element={<Overview />} />
+                    <Route path="projects" element={<ProjectsDashboard />} />
+                </Route>
+
+                <Route path="/post/:id" element={<PostPage />} />
+                <Route path="/search-projects" element={<SearchProjectsPage />} />
 
 
 
-                <Route path="*" element={<NotFound/>}/>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     )
--- frontend/src/components/pages/dashboard/dashboard-navbar.tsx
+++ frontend/src/components/pages/dashboard/dashboard-navbar.tsx
@@ -3,6 +3,13 @@ import { FaHome } from "react-icons/fa";
 import { NavbarRight } from "../../layout/navbar/NavbarRight";
 import type { JwtUser } from "../../../redux/AuthSlice";
 import logo from "../../../assets/logolight.png";
+import { NavLink } from "react-router-dom";
+
+
+const baseClasses =
+    "cursor-pointer text-gray-300 w-full p-2 inline-flex items-center gap-2 rounded-md transition";
+const activeClasses = "bg-gray-950 text-white font-bolder";
+const inactiveClasses = "hover:bg-slate-800/50 text-gray-300";
 
 interface DashboardNavbarProps {
     user: JwtUser | null
@@ -11,26 +18,37 @@ interface DashboardNavbarProps {
 export function DashboardNavbar({ user }: DashboardNavbarProps) {
     return (
         <div className="h-30 flex justify-between gap-5 items-center px-10 text-white">
-            
+
             <img src={logo} alt="logo" className="w-30 aspect-square object-fit object-center" />
-            
+
             <ul className="flex items-center gap-3 w-full p-5 font-medium text-sm tracking-wider">
 
                 <li>
-                    <button className="cursor-pointer w-full p-2 inline-flex items-center gap-2 hover:bg-slate-800/50">
+                    <NavLink
+                        end
+                        to={`/dashboard/${user?.id}`}
+                        className={({ isActive }) =>
+                            `${baseClasses} ${isActive ? activeClasses : inactiveClasses}`
+                        }
+                    >
                         <FaHome size={25} />
                         <p>Dashboard</p>
-                    </button>
+                    </NavLink>
                 </li>
 
                 <li>
-                    <button className="cursor-pointer w-full p-2 inline-flex items-center gap-2 hover:bg-slate-800/50">
+                    <NavLink
+                        to={`/dashboard/${user?.id}/projects`}
+                        className={({ isActive }) =>
+                            `${baseClasses} ${isActive ? activeClasses : inactiveClasses}`
+                        }
+                    >
                         <BiTask size={25} />
                         <p>Projects</p>
-                    </button>
+                    </NavLink>
                 </li>
 
-                
+
             </ul>
 
             <NavbarRight user={user} />
